4ae718ddcfe6c09cff6373052d4d9429638126df
Nikolay Markhotskyi <marhotsky@gmail.com>
2023-04-04 23:45:09 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,83 +1,55 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { GlobalStyle } from './Layout/GlobalStyle';
 import { Container } from './Layout/Container.styled';
 import { Filter } from './Contacts/ContactFilter/Filter';
 import { ContactForm } from './Contacts/ContactForm/ContactForm';
 import { ContactList } from './Contacts/ContactList/ContactList';
+import { useLocalStorage } from 'hooks/useLocalStorage';
 
-export class App extends Component {
-  state = {
-    contacts: [],
-    filter: '',
-  };
+export const App = () => {
+  const [contacts, setContacts] = useLocalStorage('contacts', []);
+  const [filter, setFilter] = useState('');
 
-  componentDidMount() {
-    const contacts = localStorage.getItem('contacts');
-    const parsedContacts = JSON.parse(contacts);
-
-    parsedContacts && this.setState({ contacts: parsedContacts });
-  }
-
-  componentDidUpdate(_, prevState) {
-    const { contacts } = this.state;
-
-    contacts !== prevState.contacts &&
-      localStorage.setItem('contacts', JSON.stringify(contacts));
-  }
-
-  addNewContacts = (newContact, { action }) => {
-    const { contacts } = this.state;
-
-    const duplicateName = contacts.map(el => el.name.toLowerCase());
-
-    return duplicateName.includes(newContact.name.toLowerCase())
-      ? alert(`${newContact.name} is already in contacts.`)
-      : this.setState(prevState => {
-          action.resetForm();
-          return {
-            contacts: [...prevState.contacts, newContact],
-          };
-        });
-  };
-
-  onDeleteContacts = id => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(el => el.id !== id),
-    }));
-  };
-
-  changeFilter = e => {
-    this.setState({ filter: e.target.value });
-  };
-
-  getVisibleContact = () => {
-    const { contacts, filter } = this.state;
-
-    const normalizeFilter = filter.toLocaleLowerCase();
-
-    return contacts.filter(contact =>
-      contact.name.toLocaleLowerCase().includes(normalizeFilter)
+  const addNewContacts = (newContact, { action }) => {
+    const duplicateName = contacts.find(
+      ({ name }) => name.toLowerCase() === newContact.name.toLowerCase()
     );
-  };
-
-  render() {
-    const { filter } = this.state;
-
-    const visibleContacts = this.getVisibleContact();
 
     return (
-      <Container>
-        <h1>Phonebook</h1>
-        <ContactForm onSubmit={this.addNewContacts} />
-
-        <h2>Contacts</h2>
-        <Filter value={filter} onChange={this.changeFilter} />
-        <ContactList
-          contacts={visibleContacts}
-          deleteContact={this.onDeleteContacts}
-        />
-        <GlobalStyle />
-      </Container>
+      duplicateName
+        ? alert(`${newContact.name} is already in contacts.`)
+        : setContacts(prevContacts => [...prevContacts, newContact]),
+      action.resetForm()
     );
-  }
-}
+  };
+
+  const onDeleteContacts = id => {
+    setContacts(prevContacts => prevContacts.filter(el => el.id !== id));
+  };
+
+  const changeFilter = ({ target: { value } }) => {
+    setFilter(value);
+  };
+
+  const getVisibleContact = () => {
+    const normalizeFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizeFilter)
+    );
+  };
+
+  return (
+    <Container>
+      <h1>Phonebook</h1>
+      <ContactForm onSubmit={addNewContacts} />
+
+      <h2>Contacts</h2>
+      <Filter value={filter} onChange={changeFilter} />
+      <ContactList
+        contacts={getVisibleContact()}
+        deleteContact={onDeleteContacts}
+      />
+      <GlobalStyle />
+    </Container>
+  );
+};
